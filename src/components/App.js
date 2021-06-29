3929c322a2b8b462e11ad4765fd2510ef84ab8e7
cocktailcoder <iokreddit@protonmail.com>
2021-06-29 16:13:17 -0400

    All but image relative path

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import NavBar from "./NavBar"
+import Home from "./Home"
+import About from "./About"
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home 
+      username = "Liza"
+      city = "New York"
+      />
+      <About 
+      image = "../../images/demo.png"/>
     </div>
   );
 }
